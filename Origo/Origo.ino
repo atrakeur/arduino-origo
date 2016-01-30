854912273c27e86d2818424b35d5d34893d1ae80
atrakeur <atrakeur@gmail.com>
2016-01-30 12:05:18 +0100

    feature: Initial commit

---
+++ Origo/Origo.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Origo.ino
+ Created:	30/01/2016 12:02:53
+ Author:	Valentin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
